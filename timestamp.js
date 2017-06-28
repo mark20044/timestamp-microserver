33de9302dbf972564d5606237729edeb4ea19129
System <none>
2017-06-28 22:01:17 +0000

    Checkpoint

--- timestamp.js
+++ timestamp.js
@@ -1,3 +1,5 @@
+module.exports = param 
+
 var http = require("http");
 var url = require("url");
 
@@ -5,10 +7,13 @@ var server = http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'application/json' });
   var query = url.parse(req.url, true);
   var param = decodeURI(query.pathname.slice(1));
-  var unixTime, naturalTime;
-  
-  if ( param )
+  var date = new Date(param);
+
+  var unixTime = param != "Invalid Date" ? date.getTime() : null; 
+  var naturalTime = param != "Invalid Date"? date : null;
   
   var output = { unixtime: unixTime, natural: naturalTime };  
   res.end(output);
 });
+
+server.listen(80);
